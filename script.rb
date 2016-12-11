f76d9b32b9c0b52ee071f53106e92c6c1525b5d7
sorefull <olegh.cherednichenko@gmail.com>
2016-12-11 17:12:57 +0200

    first commit with basics

---
+++ script.rb
@@ -0,0 +1,17 @@
+require 'telegram/bot'
+require 'yaml'
+
+# Setting ENV variables from application.yml
+YAML::load_file(File.join(__dir__, 'application.yml')).each do |key, val|
+  ENV[key.to_s] = val.to_s
+end
+
+# Telegram bot
+Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN']) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    end
+  end
+end
